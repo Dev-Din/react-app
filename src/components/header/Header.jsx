1d85f67131cb021dac19c7501eb9da43d011cac9
nurdinhajo <nurudiin222@gmail.com>
2023-05-29 09:27:36 +0300

    useState

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
     const time = new Date();
     const hour = time.getHours().toString().padStart(2, '0');
     const minute = time.getMinutes().toString().padStart(2, '0');
-    return `${hour}:${minute}`; 
+    return `${hour}:${minute}`;
   }
   
    
