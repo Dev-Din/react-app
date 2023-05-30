d8f88e7f83ce9eda08aa691661b4abf8eb407b40
nurdinhajo <nurudiin222@gmail.com>
2023-05-30 19:28:46 +0300

    Updated Components

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./content.css"
+import { FaTrashAlt } from 'react-icons/fa'
 function Content() {
+
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Item 1"
+    }, 
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+    }, {
+      id: 3,
+      checked: false,
+      item: "Item 1"
+    }, {
+      id: 4,
+      checked: false,
+      item: "Item 1"
+    },
+  ]);
+
+
+
   return (
     <main>
-      <h1>Content</h1>
+     <ul>
+      {items.map((item) =>(
+        <li className='item' key={item.id}>
+          <input type="checkbox"
+            checked = {item.checked}
+           />
+           <label>{item.item}</label>
+           <FaTrashAlt
+            role='button'
+            tabIndex={0}
+            />
+        </li>
+      ))}
+     </ul>
     </main>
   )
 }
